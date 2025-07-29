af74af04b00af2918dac85a8ac003b328a21b7fb
Venkat Raju <118790430+venkat2295@users.noreply.github.com>
2025-07-29 21:17:56 +0530

    1 question

---
+++ Stacks-and-Queues/StockSpan.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+int main(){
+    vector<int> prices = {100, 80, 60, 70, 60, 75, 85};
+    vector<int> ans(prices.size(), 0);
+    stack<int> s;
+
+    for(int i=0;i<prices.size();i++){
+        while(!s.empty()&& prices[s.top()]<=prices[i]){
+            s.pop();
+        }
+        if(s.empty()){
+            ans[i] = i+1;
+        }else{
+            ans[i]= i -s.top();
+        }
+        s.push(i);
+    }
+
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+}
