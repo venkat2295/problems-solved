7c64523ccce49b83dced69d7c92b6ef3f9af308c
Venkat Raju <118790430+venkat2295@users.noreply.github.com>
2025-07-31 11:33:57 +0530

    Time: 3 ms (62.01%), Space: 23.6 MB (31.29%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,30 +1,45 @@
 class MinStack {
 public:
-stack<pair<int,int>>s;
+stack<long long int>s;
+long long int minVal;
     MinStack() {
         
     }
     
     void push(int val) {
-        if(s.empty()){
-            s.push({val,val});
-        }else{
-            int minVal = min(val,s.top().second);
-            s.push({val,minVal});
-        }
+       if(s.empty()){
+        s.push(val);
+        minVal=val;
+       }else{
+         if(val<minVal){
+        s.push((long long)2*val-minVal);
+        minVal=val;
+       }else{
+        s.push(val);
+       }
+       }
+      
     }
     
     void pop() {
+        if(s.top()<minVal){
+            minVal=2*minVal-s.top();
+        }
         s.pop();
         
     }
     
     int top() {
-        return s.top().first;
+        if(s.top()<minVal){
+            return minVal;
+
+        }
+        return s.top();
+        
     }
     
     int getMin() {
-        return s.top().second;
+        return minVal;
     }
 };
 
