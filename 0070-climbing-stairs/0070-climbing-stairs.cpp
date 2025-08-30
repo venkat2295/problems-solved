8aecabff1aa5789b5a1c198d8a7936e76cf7f46f
Venkat Raju <118790430+venkat2295@users.noreply.github.com>
2025-08-30 12:26:39 +0530

    Time: 2 ms (5.22%), Space: 8.6 MB (18.69%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+private:
+ int func(int n,vector<int>&dp){
+    if(n<=1 ){
+        return 1;
+
+    }
+    if(dp[n]!=-1){
+        return dp[n];
+    }
+
+    return dp[n]=func(n-1,dp)+func(n-2,dp);
+ }
+public:
+    int climbStairs(int n) {
+       vector<int>dp(n+1,-1);
+       return func(n,dp); 
+    }
+};
